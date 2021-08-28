2a42728291124ccad014bbecd13b3353b8f3cbd9
dont.mail.me.any.spam@gmail.com <dont.mail.me.any.spam@gmail.com>
2021-08-28 16:13:42 -0400

    Add project files.

---
+++ ASMFileRenamer/Form2.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace ASMFileRenamer
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvASMRenamed = new System.Windows.Forms.DataGridView();
+            this.Enable = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.OriginalName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NewName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnOpenAsmXMLFile = new System.Windows.Forms.Button();
+            this.btnSaveRenamingFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCurrentFile = new System.Windows.Forms.Label();
+            this.tboxSearchBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClearNew = new System.Windows.Forms.Button();
+            this.btnLoadASMFile = new System.Windows.Forms.Button();
+            this.btnSaveRenamedASM = new System.Windows.Forms.Button();
+            this.btnOpenLabelFile = new System.Windows.Forms.Button();
+            this.cboxReverseColumns = new System.Windows.Forms.CheckBox();
+            this.cboxIgnoreLabelStrings = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvASMRenamed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvASMRenamed
+            // 
+            this.dgvASMRenamed.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvASMRenamed.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Enable,
+            this.OriginalName,
+            this.NewName});
+            this.dgvASMRenamed.Location = new System.Drawing.Point(12, 76);
+            this.dgvASMRenamed.Name = "dgvASMRenamed";
+            this.dgvASMRenamed.Size = new System.Drawing.Size(713, 392);
+            this.dgvASMRenamed.TabIndex = 0;
+            // 
+            // Enable
+            // 
+            this.Enable.HeaderText = "Enable";
+            this.Enable.Name = "Enable";
+            this.Enable.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Enable.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Enable.Width = 50;
+            // 
+            // OriginalName
+            // 
+            this.OriginalName.HeaderText = "OriginalName";
+            this.OriginalName.Name = "OriginalName";
+            this.OriginalName.Width = 200;
+            // 
+            // NewName
+            // 
+            this.NewName.HeaderText = "NewName";
+            this.NewName.Name = "NewName";
+            this.NewName.Width = 400;
+            // 
+            // btnOpenAsmXMLFile
+            // 
+            this.btnOpenAsmXMLFile.Location = new System.Drawing.Point(30, 486);
+            this.btnOpenAsmXMLFile.Name = "btnOpenAsmXMLFile";
+            this.btnOpenAsmXMLFile.Size = new System.Drawing.Size(137, 23);
+            this.btnOpenAsmXMLFile.TabIndex = 1;
+            this.btnOpenAsmXMLFile.Text = "Open XML Renaming File";
+            this.btnOpenAsmXMLFile.UseVisualStyleBackColor = true;
+            this.btnOpenAsmXMLFile.Click += new System.EventHandler(this.btnOpenAsmXMLFile_Click);
+            // 
+            // btnSaveRenamingFile
+            // 
+            this.btnSaveRenamingFile.Location = new System.Drawing.Point(175, 486);
+            this.btnSaveRenamingFile.Name = "btnSaveRenamingFile";
+            this.btnSaveRenamingFile.Size = new System.Drawing.Size(113, 23);
+            this.btnSaveRenamingFile.TabIndex = 2;
+            this.btnSaveRenamingFile.Text = "Save AS";
+            this.btnSaveRenamingFile.UseVisualStyleBackColor = true;
+            this.btnSaveRenamingFile.Click += new System.EventHandler(this.btnSaveRenamingFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Current File:";
+            // 
+            // labelCurrentFile
+            // 
+            this.labelCurrentFile.AutoSize = true;
+            this.labelCurrentFile.Location = new System.Drawing.Point(122, 9);
+            this.labelCurrentFile.Name = "labelCurrentFile";
+            this.labelCurrentFile.Size = new System.Drawing.Size(0, 13);
+            this.labelCurrentFile.TabIndex = 4;
+            // 
+            // tboxSearchBox
+            // 
+            this.tboxSearchBox.Location = new System.Drawing.Point(120, 39);
+            this.tboxSearchBox.Name = "tboxSearchBox";
+            this.tboxSearchBox.Size = new System.Drawing.Size(288, 20);
+            this.tboxSearchBox.TabIndex = 5;
+            this.tboxSearchBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tboxSearchBox_KeyUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Search:";
+            // 
+            // btnClearNew
+            // 
+            this.btnClearNew.Location = new System.Drawing.Point(650, 32);
+            this.btnClearNew.Name = "btnClearNew";
+            this.btnClearNew.Size = new System.Drawing.Size(75, 23);
+            this.btnClearNew.TabIndex = 7;
+            this.btnClearNew.Text = "Clear \\ New";
+            this.btnClearNew.UseVisualStyleBackColor = true;
+            this.btnClearNew.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLoadASMFile
+            // 
+            this.btnLoadASMFile.Location = new System.Drawing.Point(336, 486);
+            this.btnLoadASMFile.Name = "btnLoadASMFile";
+            this.btnLoadASMFile.Size = new System.Drawing.Size(113, 23);
+            this.btnLoadASMFile.TabIndex = 8;
+            this.btnLoadASMFile.Text = "Load ASM File";
+            this.btnLoadASMFile.UseVisualStyleBackColor = true;
+            this.btnLoadASMFile.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSaveRenamedASM
+            // 
+            this.btnSaveRenamedASM.Location = new System.Drawing.Point(455, 486);
+            this.btnSaveRenamedASM.Name = "btnSaveRenamedASM";
+            this.btnSaveRenamedASM.Size = new System.Drawing.Size(132, 23);
+            this.btnSaveRenamedASM.TabIndex = 9;
+            this.btnSaveRenamedASM.Text = "Save Renamed ASM";
+            this.btnSaveRenamedASM.UseVisualStyleBackColor = true;
+            this.btnSaveRenamedASM.Click += new System.EventHandler(this.btnSaveRenamedASM_Click);
+            // 
+            // btnOpenLabelFile
+            // 
+            this.btnOpenLabelFile.Location = new System.Drawing.Point(30, 516);
+            this.btnOpenLabelFile.Name = "btnOpenLabelFile";
+            this.btnOpenLabelFile.Size = new System.Drawing.Size(137, 23);
+            this.btnOpenLabelFile.TabIndex = 10;
+            this.btnOpenLabelFile.Text = "Import asm662 label file";
+            this.btnOpenLabelFile.UseVisualStyleBackColor = true;
+            this.btnOpenLabelFile.Click += new System.EventHandler(this.btnOpenLabelFile_Click);
+            // 
+            // cboxReverseColumns
+            // 
+            this.cboxReverseColumns.AutoSize = true;
+            this.cboxReverseColumns.Location = new System.Drawing.Point(175, 520);
+            this.cboxReverseColumns.Name = "cboxReverseColumns";
+            this.cboxReverseColumns.Size = new System.Drawing.Size(158, 17);
+            this.cboxReverseColumns.TabIndex = 11;
+            this.cboxReverseColumns.Text = "Reverse columns upon load";
+            this.cboxReverseColumns.UseVisualStyleBackColor = true;
+            // 
+            // cboxIgnoreLabelStrings
+            // 
+            this.cboxIgnoreLabelStrings.AutoSize = true;
+            this.cboxIgnoreLabelStrings.Location = new System.Drawing.Point(349, 520);
+            this.cboxIgnoreLabelStrings.Name = "cboxIgnoreLabelStrings";
+            this.cboxIgnoreLabelStrings.Size = new System.Drawing.Size(229, 17);
+            this.cboxIgnoreLabelStrings.TabIndex = 12;
+            this.cboxIgnoreLabelStrings.Text = "Ignore \"label\" NewName Strings upon load";
+            this.cboxIgnoreLabelStrings.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 545);
+            this.Controls.Add(this.cboxIgnoreLabelStrings);
+            this.Controls.Add(this.cboxReverseColumns);
+            this.Controls.Add(this.btnOpenLabelFile);
+            this.Controls.Add(this.btnSaveRenamedASM);
+            this.Controls.Add(this.btnLoadASMFile);
+            this.Controls.Add(this.btnClearNew);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tboxSearchBox);
+            this.Controls.Add(this.labelCurrentFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSaveRenamingFile);
+            this.Controls.Add(this.btnOpenAsmXMLFile);
+            this.Controls.Add(this.dgvASMRenamed);
+            this.Name = "Form2";
+            this.Text = "ASM File Renamer v1.1";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvASMRenamed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvASMRenamed;
+        private System.Windows.Forms.Button btnOpenAsmXMLFile;
+        private System.Windows.Forms.Button btnSaveRenamingFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCurrentFile;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Enable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OriginalName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NewName;
+        private System.Windows.Forms.TextBox tboxSearchBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClearNew;
+        private System.Windows.Forms.Button btnLoadASMFile;
+        private System.Windows.Forms.Button btnSaveRenamedASM;
+        private System.Windows.Forms.Button btnOpenLabelFile;
+        private System.Windows.Forms.CheckBox cboxReverseColumns;
+        private System.Windows.Forms.CheckBox cboxIgnoreLabelStrings;
+    }
+}
---
+++ ASMFileRenamer/Form2.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace ASMFileRenamer
+{
+       
+    
+    public partial class Form2 : Form
+    {
+
+        public Form2()
+        {
+            
+            InitializeComponent();
+            cboxIgnoreLabelStrings.Checked = true;
+            
+
+        }
+//        string OpenedXMLFile;
+        DataSet dataset1 = new DataSet();
+        DataView dv = new DataView();
+        bool FirstRun = true;
+        string XMLPath;
+
+        private void MakeDataSet()
+        {
+            dataset1.ReadXml(XMLPath);
+            dgvASMRenamed.AutoGenerateColumns = false;
+            if (cboxReverseColumns.Checked)
+            {
+                dgvASMRenamed.Columns[0].DataPropertyName = "Enable";
+                dgvASMRenamed.Columns[2].DataPropertyName = "OriginalName";
+                dgvASMRenamed.Columns[1].DataPropertyName = "NewName";
+            }
+            else
+            {
+                dgvASMRenamed.Columns[0].DataPropertyName = "Enable";
+                dgvASMRenamed.Columns[1].DataPropertyName = "OriginalName";
+                dgvASMRenamed.Columns[2].DataPropertyName = "NewName";
+            }
+
+            dv.Table = dataset1.Tables[0];
+            dgvASMRenamed.DataSource = dv;
+        }
+        OpenFileDialog OPENAsmRenamingFile = new OpenFileDialog();
+        private void btnOpenAsmXMLFile_Click(object sender, EventArgs e)
+        {
+           
+            OPENAsmRenamingFile.Filter = "XML Files | *.xml";
+            if (OPENAsmRenamingFile.ShowDialog() == DialogResult.OK)
+            {
+                XMLPath = OPENAsmRenamingFile.FileName;
+                dgvASMRenamed.DataSource = null;
+                dgvASMRenamed.Rows.Clear();
+                tboxSearchBox.Text = "";
+                dv.RowFilter = null;
+                if (dv.Table != null)
+                {
+                    dv.Table.Clear();
+                    this.dgvASMRenamed.DataSource = null;
+                    dgvASMRenamed.Rows.Clear();
+                }
+
+                //OpenedXMLFile = OPENAsmRenamingFile.FileName;
+
+                try
+                {
+                    labelCurrentFile.Text = OPENAsmRenamingFile.FileName;
+                    MakeDataSet();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+        }
+
+
+        DataTable DT = new DataTable();
+        private DataTable GetDataTableFromDGV(DataGridView dgv)
+        {
+
+            int j = 0;
+            foreach(DataGridViewColumn col in dgv.Columns)
+            {
+                if (col.Visible)
+                {
+                    string headerText = dgv.Columns[j].HeaderText;
+                    headerText = System.Text.RegularExpressions.Regex.Replace(headerText, "[-/, ]", "_");
+                    if (FirstRun)
+                    {
+                        DataColumn column = new DataColumn(headerText);
+                        DT.Columns.Add(column);
+                    }
+
+                    j++;
+                }
+            }
+
+            object[] cellValues = new object[dgv.Columns.Count];
+            foreach(DataGridViewRow row in dgv.Rows)
+            {
+                for(int i = 0; i < row.Cells.Count; i++)
+                {
+                    cellValues[i] = row.Cells[i].Value;
+                }
+                DT.Rows.Add(cellValues);
+            }
+            FirstRun = false;
+            return DT;
+        }
+
+        SaveFileDialog SaveAsmRenamingFile = new SaveFileDialog();
+        DataSet DS = new DataSet();
+        private void btnSaveRenamingFile_Click(object sender, EventArgs e)
+        {
+                SaveAsmRenamingFile.Filter = "XML Files | *.xml";
+                if (SaveAsmRenamingFile.ShowDialog() == DialogResult.OK)
+                {
+                labelCurrentFile.Text = SaveAsmRenamingFile.FileName;
+                //OpenedXMLFile = SaveAsmRenamingFile.FileName;
+                XMLPath = SaveAsmRenamingFile.FileName;
+                tboxSearchBox.Text = "";
+                dv.RowFilter = null;
+                DT.Clear();
+                DT = GetDataTableFromDGV(dgvASMRenamed);
+                if (DS.Tables.CanRemove(DT))
+                {
+                    DS.Tables.Remove(DT);
+                }
+                DS.Clear();
+                DS.Tables.Add(DT);
+                
+               
+                              
+                // Create the FileStream to write with.
+                System.IO.FileStream stream = new System.IO.FileStream
+                    //(OpenedXMLFile, System.IO.FileMode.Create);
+                    (XMLPath, System.IO.FileMode.Create);
+
+                // Create an XmlTextWriter with the fileStream.
+                System.Xml.XmlTextWriter xmlWriter =
+                    new System.Xml.XmlTextWriter(stream,
+                    System.Text.Encoding.Unicode);
+
+                // Write to the file with the WriteXml method.
+                DS.WriteXml(xmlWriter);
+                xmlWriter.Close();
+
+                }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tboxSearchBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            dv.RowFilter = "OriginalName Like '%" + tboxSearchBox.Text + "%' OR NewName Like '%" + tboxSearchBox.Text + "%'";
+            this.dgvASMRenamed.DataSource = dv;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dv.Table != null)
+            {
+                dv.Table.Clear();
+                this.dgvASMRenamed.DataSource = null;
+                dgvASMRenamed.Rows.Clear();
+            }
+            labelCurrentFile.Text = "";
+            //OpenedXMLFile = null;
+            XMLPath = null;
+            tboxSearchBox.Text = "";
+            dv.RowFilter = null;
+            DT.Clear();
+            DT = GetDataTableFromDGV(dgvASMRenamed);
+            if (DS.Tables.CanRemove(DT))
+            {
+                DS.Tables.Remove(DT);
+            }
+            DS.Clear();
+            DS.Tables.Add(DT);
+        }
+        
+        
+        OpenFileDialog OPENAsmDialog = new OpenFileDialog();
+        string CurrentASMPath;
+        string ASMSimpleName;
+        bool ASMOpened = false;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OPENAsmDialog.Filter = "ASM Files | *.asm";
+            if (OPENAsmDialog.ShowDialog() == DialogResult.OK)
+            {
+                CurrentASMPath = OPENAsmDialog.FileName;
+                LoadASMFileIntoStringArray();
+                ASMSimpleName = OPENAsmDialog.SafeFileName;
+                ASMOpened = true;
+            }
+        }
+
+        string[] ASMFileArray;
+        string[] RenamedASMFileArray;
+        private void LoadASMFileIntoStringArray()
+        {
+            ASMFileArray = System.IO.File.ReadAllLines(CurrentASMPath);
+        }
+
+        //StreamWriter objStreamWriter;
+        private void btnSaveRenamedASM_Click(object sender, EventArgs e)
+        {
+            if (ASMOpened)
+            {
+                try
+                {
+
+                    RenamedASMFileArray = System.IO.File.ReadAllLines(CurrentASMPath);
+                    ReadXMLFileIntoDataset();
+                    MessageBox.Show("The renamed file will be saved as RENAMED_" + ASMSimpleName+"\n Please be patient with large files and long rename lists." +
+                        "\nYou will be notified when the process is complete.");
+                    RenameUsingValuesFromXML(RenamedASMFileArray);
+                    string RenamedAsmName = System.IO.Path.GetDirectoryName(CurrentASMPath) + "\\" + "RENAMED_" + ASMSimpleName;
+                    using (StreamWriter outputfile = new StreamWriter(RenamedAsmName))
+                    {
+                        foreach (string line in RenamedASMFileArray)
+                            outputfile.WriteLine(line);
+                    }
+                    MessageBox.Show("File saved as RENAMED_" + ASMSimpleName);
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Open an ASM file that you would like to rename first.");
+            }
+
+           
+        }
+        private void ReadXMLFileIntoDataset()
+        {
+            try
+            {
+                dataset1.Clear();
+                // dataset1.ReadXml(OPENAsmRenamingFile.FileName);
+                dataset1.ReadXml(XMLPath);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+        private void RenameUsingValuesFromXML(string[] OrigArray, bool RestoreToOriginalValue = false)
+        {
+
+
+            for (int index = 0; index < OrigArray.Length; index++)
+            {
+                foreach (DataTable table in dataset1.Tables)
+                {
+                    foreach (DataRow dr in table.Rows)
+                    {
+
+                        if (RestoreToOriginalValue || cboxReverseColumns.Checked)
+                        {
+
+                            OrigArray[index] = CaseSenstiveReplace(OrigArray[index], Convert.ToString(dr[2]), Convert.ToString(dr[1]));
+                        }
+                        else if (Convert.ToString(dr[0]) == "True")
+                        {
+                            OrigArray[index] = CaseSenstiveReplace(OrigArray[index], Convert.ToString(dr[1]), Convert.ToString(dr[2]));
+                        }
+
+
+                    }
+                }
+
+            }
+
+
+        }
+        public string CaseSenstiveReplace(string originalString, string oldValue, string newValue)
+        {
+            if (oldValue != "")
+            {
+                return originalString.Replace(oldValue, newValue);
+            }
+            else
+            {
+                return originalString;
+            }
+        }
+        OpenFileDialog OPENLabelFile = new OpenFileDialog();
+        string OpenedTXTFile;
+        string[] OpenTXTFileArray;
+        string[] ModifiedTxtArray;
+        string XMLHeader = "<NewDataSet>";
+        string XMLFooter = "</NewDataSet>";
+        string XMLStringPart1 = "<Table1><Enable>True</Enable><OriginalName>";
+        string XMLStringPart2 = "</OriginalName><NewName>";
+        string XMLStringPart3 = "</NewName></Table1>";
+        string TrimmedString;
+        String XMLLabelOutput;
+        String XMLTblOutput;
+        List<string> XMLStringArray = new List<string>();
+        bool SkipThisString;
+        string strNewNameStringPrefix;
+
+
+
+        private void btnOpenLabelFile_Click(object sender, EventArgs e)
+        {
+
+           
+                OPENLabelFile.Filter = "TXT Files | *.txt";
+                if (OPENLabelFile.ShowDialog() == DialogResult.OK)
+                {
+
+
+                OpenedTXTFile = OPENLabelFile.FileName;
+                OpenTXTFileArray = System.IO.File.ReadAllLines(OpenedTXTFile);
+                int i = 0;
+                foreach (string s in OpenTXTFileArray)
+                {
+                    //Remove whitespace from string
+                    string strNoWhitespace = new string(s.ToCharArray()
+                    .Where(c => !Char.IsWhiteSpace(c))
+                    .ToArray());
+                    OpenTXTFileArray[i] = strNoWhitespace;
+                   
+                    //Remove char after // 
+                int index = 0;
+                index = OpenTXTFileArray[i].IndexOf("//");
+                    if(index >= 0)
+                    {
+                        TrimmedString = OpenTXTFileArray[i].Substring(0, index);
+                    }
+                else
+                    {
+                        TrimmedString = OpenTXTFileArray[i];
+                    }
+
+                    string[] SplitArray = TrimmedString.Split('=');
+                   
+                    if (SplitArray.Length >1 && SplitArray[1].Length > 5)
+                    {
+                        string strHexAddress = SplitArray[1].Substring(2, 4);
+                        if (SplitArray[0].Length > 5)
+                        {
+                            strNewNameStringPrefix = SplitArray[0].Substring(0, 5);
+                        }
+                        
+                        strNewNameStringPrefix = strNewNameStringPrefix.ToLower();
+                        SkipThisString = false;
+                        if(strNewNameStringPrefix == "label" && cboxIgnoreLabelStrings.Checked)
+                        {
+                           SkipThisString = true;
+                        }
+                        SplitArray[1] = strHexAddress.ToLower();
+                    }
+                    
+                    try
+                    {
+                        XMLLabelOutput = XMLStringPart1 + "label_" + SplitArray[1] + XMLStringPart2 + SplitArray[0] + XMLStringPart3;
+                        XMLTblOutput = XMLStringPart1 + "tbl_" + SplitArray[1] + XMLStringPart2 + SplitArray[0] + XMLStringPart3;
+                    }
+                    catch
+                    {
+
+                    }
+                    if(i == 0)
+                    {
+                        XMLStringArray.Clear();
+                        XMLStringArray.Add(XMLHeader);
+                    }
+                    if(i == OpenTXTFileArray.Length - 3)
+                    {
+                        XMLStringArray.Add(XMLFooter);
+                        break;
+                    }
+                    else
+                    {
+
+                        if(XMLLabelOutput != null && SkipThisString != true)
+                        {
+                            XMLStringArray.Add(XMLLabelOutput);
+                        }
+                       if(XMLTblOutput != null && SkipThisString != true)
+                        {
+                            XMLStringArray.Add(XMLTblOutput);
+                        }
+                       
+                    }
+
+    
+
+
+                i++;
+                }
+
+                XMLPath = System.IO.Path.GetDirectoryName(OpenedTXTFile) + "\\" + "tempxml.xml";
+                using (StreamWriter outputfile = new StreamWriter(XMLPath))
+                {
+                    foreach (string line in XMLStringArray)
+                        outputfile.WriteLine(line);
+                }
+
+
+
+                try
+                    {
+                   // System.IO.File.Open("tempxml.xml", FileMode.Open);
+                        labelCurrentFile.Text = XMLPath;
+                    MakeDataSet();
+                    }
+                    catch (Exception error)
+                    {
+                        MessageBox.Show(error.Message);
+                    }
+                }
+            
+        }
+    }
+}
